528b0a77568da9b1b96f582472e634919c9ffbb6
Djavan Munroe <djavanm24@gmail.com>
2019-09-14 17:47:14 -0600

    Add sprites to myPC route

--- src/containers/MyPC/MyPC.js
+++ src/containers/MyPC/MyPC.js
@@ -1,11 +1,20 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
-export const MyPC =  () => {
+export const MyPC = ({ caughtPokemon }) => {
+  let sprites = caughtPokemon.map((pokemon, index) => {
+    return <img className='sprite' src={`${pokemon.sprite}`} alt={`{pokemon.name}'s sprite`} key={index} />
+  });
   return (
     <section className='my-pc-container'>
-
+      {sprites}
     </section>
   )
-}
+};
 
-export default MyPC;
+export const mapStateToProps = state => ({
+  currentPokemon: state.currentPokemon,
+  caughtPokemon: state.caughtPokemon
+});
+
+export default connect(mapStateToProps)(MyPC);
