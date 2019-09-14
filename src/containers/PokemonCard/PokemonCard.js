69103c4533a538d74f8c90e33484825a9411acf7
Djavan Munroe <djavanm24@gmail.com>
2019-09-14 12:34:50 -0600

    Add color to background and add container for pokemon

--- src/containers/PokemonCard/PokemonCard.js
+++ src/containers/PokemonCard/PokemonCard.js
@@ -5,8 +5,9 @@ export const PokemonCard = ({ currentPokemon }) => {
   const { name, id, types, height, weight, image, gen } = currentPokemon;
   let typeInfo = types ? types.map((type, index) => <li key={index}>{type}</li>) : null;
   return (
-    <article>
-      <img src={image} alt={`name's card`} data-id={id} />
+    <article className="card-container">
+      <img className="poke-img" src={image} alt={`name's card`} data-id={id} />
+      <div className="poke-info">
       <p>{name}</p>
       <p>Type{types.length > 1 && <span>s</span>}</p>
       <ul>
@@ -15,6 +16,7 @@ export const PokemonCard = ({ currentPokemon }) => {
       <p>Height: {height}</p>
       <p>Weight: {weight}</p>
       <p>Gen: {gen}</p>
+      </div>
     </article>
   )
 }
