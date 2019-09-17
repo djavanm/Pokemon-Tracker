f31a8b545ca390ea506ac5d85a14dbd44dec440e
Djavan Munroe <djavanm24@gmail.com>
2019-09-16 18:49:12 -0600

    Add SearchForm testing

---
+++ src/containers/SearchForm/SearchForm.test.js
@@ -0,0 +1,78 @@
+import React, { Component } from 'react';
+import { shallow } from 'enzyme';
+import { SearchForm, mapStateToProps, mapDispatchToProps } from './SearchForm';
+import { getPokemon, getDescription } from '../../util/apiCalls';
+import { setPokemon } from '../../actions';
+import { Link } from 'react-router-dom';
+
+describe('SearchForm', () => {
+  let wrapper;
+  const mockPokemon = {
+    name: 'Bulbasaur',
+    description: 'Bulbasaur is cool.',
+    gen: 'Generation 1',
+    gif: 'https://projectpokemon.org/images/normal-sprite/bulbasaur.gif',
+    height: 7,
+    id: 1,
+    image: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png',
+    sprite: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png'
+  };
+
+  const mockCaughtPokemon = [mockPokemon];
+  const mockTeamPokemon = [mockPokemon];
+  const mockCurrentPokemon =  mockPokemon;
+  const mockState = {
+    currentPokemon: mockPokemon,
+    caughtPokemon: mockCaughtPokemon,
+    teamPokemon: mockTeamPokemon
+  };
+
+  beforeEach(() => {
+    wrapper = shallow(<SearchForm
+      pokemon={mockCurrentPokemon}
+      caughtPokemon={mockCaughtPokemon}
+      />)
+  });
+
+  it('should match the snapshot given the proper information', () => {
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it('should update the state when handle change is called', () => {
+    const mockEventName = {
+      target: {
+        name: 'name',
+        value: 'squirtle'
+      }
+    };
+    const mockEventId= {
+      target: {
+        name: 'id',
+        value: 4
+      }
+    };
+    const expectedName = 'squirtle';
+    const expectedId = 4;
+    wrapper.instance().handleChange(mockEventName);
+    wrapper.instance().handleChange(mockEventId);
+    expect(wrapper.state('name')).toEqual(expectedName);
+    expect(wrapper.state('id')).toEqual(expectedId);
+  });
+
+  it('mapStateToProps should grab the props it needs', () => {
+    const expected = {
+      pokemon: mockPokemon,
+      caughtPokemon: mockCaughtPokemon
+    };
+    const mappedProps = mapStateToProps(mockState);
+    expect(mappedProps).toEqual(expected);
+  });
+
+  it('it calls dispatch with the setPokemon action', () => {
+    const mockDispatch = jest.fn();
+    const actionToDispatch = setPokemon(mockPokemon);
+    const mappedProps = mapDispatchToProps(mockDispatch);
+    mappedProps.setPokemon(mockPokemon);
+    expect(mockDispatch).toHaveBeenCalledWith(actionToDispatch);
+  });
+});
